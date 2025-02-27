bea9f5d44c24ba8dc77c9a540100ab603447a6a7
Uriel <garuriel.120@gmail.com>
2025-02-26 21:42:46 -0600

    Agregué dependencias

--- FilmManager/obj/Debug/net9.0-windows/FilmManager.AssemblyInfo.cs
+++ FilmManager/obj/Debug/net9.0-windows/FilmManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84896fd9ea8cba9c6e84cf5b2bb763f22fd9d298")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
